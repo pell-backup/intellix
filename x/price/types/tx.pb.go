e0867a13a0c2919b24d553267bfff41de8810559
kai <kai@>
2024-10-24 15:44:09 +0800

    refactor: change MsgVoteRequestPriceFeed & MsgVoteFinalizedRequestPrice

--- x/price/types/tx.pb.go
+++ x/price/types/tx.pb.go
@@ -6,10 +6,6 @@ package types
 import (
 	context "context"
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-
 	_ "github.com/cosmos/cosmos-proto"
 	_ "github.com/cosmos/cosmos-sdk/types/msgservice"
 	_ "github.com/cosmos/cosmos-sdk/types/tx/amino"
@@ -19,6 +15,9 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -127,35 +126,117 @@ func (m *MsgUpdateParamsResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgUpdateParamsResponse proto.InternalMessageInfo
 
+type MsgVoteRequestPriceFeedResponse struct {
+}
+
+func (m *MsgVoteRequestPriceFeedResponse) Reset()         { *m = MsgVoteRequestPriceFeedResponse{} }
+func (m *MsgVoteRequestPriceFeedResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgVoteRequestPriceFeedResponse) ProtoMessage()    {}
+func (*MsgVoteRequestPriceFeedResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d83991210d1f4d99, []int{2}
+}
+func (m *MsgVoteRequestPriceFeedResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgVoteRequestPriceFeedResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgVoteRequestPriceFeedResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgVoteRequestPriceFeedResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgVoteRequestPriceFeedResponse.Merge(m, src)
+}
+func (m *MsgVoteRequestPriceFeedResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgVoteRequestPriceFeedResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgVoteRequestPriceFeedResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgVoteRequestPriceFeedResponse proto.InternalMessageInfo
+
+type MsgVoteFinalizedRequestPriceResponse struct {
+}
+
+func (m *MsgVoteFinalizedRequestPriceResponse) Reset()         { *m = MsgVoteFinalizedRequestPriceResponse{} }
+func (m *MsgVoteFinalizedRequestPriceResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgVoteFinalizedRequestPriceResponse) ProtoMessage()    {}
+func (*MsgVoteFinalizedRequestPriceResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d83991210d1f4d99, []int{3}
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgVoteFinalizedRequestPriceResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgVoteFinalizedRequestPriceResponse.Merge(m, src)
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgVoteFinalizedRequestPriceResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgVoteFinalizedRequestPriceResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgUpdateParams)(nil), "intellix.price.MsgUpdateParams")
 	proto.RegisterType((*MsgUpdateParamsResponse)(nil), "intellix.price.MsgUpdateParamsResponse")
+	proto.RegisterType((*MsgVoteRequestPriceFeedResponse)(nil), "intellix.price.MsgVoteRequestPriceFeedResponse")
+	proto.RegisterType((*MsgVoteFinalizedRequestPriceResponse)(nil), "intellix.price.MsgVoteFinalizedRequestPriceResponse")
 }
 
 func init() { proto.RegisterFile("intellix/price/tx.proto", fileDescriptor_d83991210d1f4d99) }
 
 var fileDescriptor_d83991210d1f4d99 = []byte{
-	// 320 bytes of a gzipped FileDescriptorProto
+	// 437 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcf, 0xcc, 0x2b, 0x49,
 	0xcd, 0xc9, 0xc9, 0xac, 0xd0, 0x2f, 0x28, 0xca, 0x4c, 0x4e, 0xd5, 0x2f, 0xa9, 0xd0, 0x2b, 0x28,
 	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x83, 0x49, 0xe8, 0x81, 0x25, 0xa4, 0x04, 0x13, 0x73, 0x33, 0xf3,
 	0xf2, 0xf5, 0xc1, 0x24, 0x44, 0x89, 0x94, 0x78, 0x72, 0x7e, 0x71, 0x6e, 0x7e, 0xb1, 0x7e, 0x6e,
 	0x71, 0xba, 0x7e, 0x99, 0x21, 0x88, 0x82, 0x4a, 0x48, 0x42, 0x24, 0xe2, 0xc1, 0x3c, 0x7d, 0x08,
 	0x07, 0x2a, 0x25, 0x92, 0x9e, 0x9f, 0x9e, 0x0f, 0x11, 0x07, 0xb1, 0xa0, 0xa2, 0xd2, 0x68, 0xae,
-	0x28, 0x48, 0x2c, 0x4a, 0xcc, 0x85, 0x6a, 0x51, 0xda, 0xc9, 0xc8, 0xc5, 0xef, 0x5b, 0x9c, 0x1e,
-	0x5a, 0x90, 0x92, 0x58, 0x92, 0x1a, 0x00, 0x96, 0x11, 0x32, 0xe3, 0xe2, 0x4c, 0x2c, 0x2d, 0xc9,
-	0xc8, 0x2f, 0xca, 0x2c, 0xa9, 0x94, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x74, 0x92, 0xb8, 0xb4, 0x45,
-	0x57, 0x04, 0x6a, 0x97, 0x63, 0x4a, 0x4a, 0x51, 0x6a, 0x71, 0x71, 0x70, 0x49, 0x51, 0x66, 0x5e,
-	0x7a, 0x10, 0x42, 0xa9, 0x90, 0x25, 0x17, 0x1b, 0xc4, 0x6c, 0x09, 0x26, 0x05, 0x46, 0x0d, 0x6e,
-	0x23, 0x31, 0x3d, 0x54, 0x6f, 0xea, 0x41, 0xcc, 0x77, 0xe2, 0x3c, 0x71, 0x4f, 0x9e, 0x61, 0xc5,
-	0xf3, 0x0d, 0x5a, 0x8c, 0x41, 0x50, 0x0d, 0x56, 0xc6, 0x4d, 0xcf, 0x37, 0x68, 0x21, 0x8c, 0xea,
-	0x7a, 0xbe, 0x41, 0x4b, 0x01, 0xee, 0x6c, 0x98, 0xc3, 0xd1, 0xdc, 0xa9, 0x24, 0xc9, 0x25, 0x8e,
-	0x26, 0x14, 0x94, 0x5a, 0x5c, 0x90, 0x9f, 0x57, 0x9c, 0x6a, 0x94, 0xc6, 0xc5, 0xec, 0x5b, 0x9c,
-	0x2e, 0x14, 0xc1, 0xc5, 0x83, 0xe2, 0x33, 0x79, 0x74, 0x17, 0xa1, 0xe9, 0x97, 0x52, 0x27, 0xa0,
-	0x00, 0x66, 0x81, 0x14, 0x6b, 0x03, 0xc8, 0xfd, 0x4e, 0x06, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78,
-	0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc,
-	0x78, 0x2c, 0xc7, 0x10, 0x25, 0x86, 0xe1, 0xfc, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24, 0x36, 0x70,
-	0xb8, 0x1b, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xb5, 0xc9, 0xae, 0x48, 0x1c, 0x02, 0x00, 0x00,
+	0x28, 0x48, 0x2c, 0x4a, 0xcc, 0x85, 0x69, 0xd1, 0x41, 0x93, 0x2c, 0xcb, 0x2f, 0x49, 0x8d, 0x2f,
+	0x4a, 0x2d, 0x2c, 0x4d, 0x2d, 0x2e, 0x89, 0x07, 0x0b, 0xc5, 0xa7, 0xa5, 0xa6, 0xa6, 0x40, 0x55,
+	0x1b, 0x62, 0x53, 0x9d, 0x96, 0x99, 0x97, 0x98, 0x93, 0x59, 0x95, 0x9a, 0x82, 0xaa, 0x0f, 0xa2,
+	0x45, 0x69, 0x27, 0x23, 0x17, 0xbf, 0x6f, 0x71, 0x7a, 0x68, 0x41, 0x4a, 0x62, 0x49, 0x6a, 0x00,
+	0xd8, 0x6a, 0x21, 0x33, 0x2e, 0xce, 0xc4, 0xd2, 0x92, 0x8c, 0xfc, 0xa2, 0xcc, 0x92, 0x4a, 0x09,
+	0x46, 0x05, 0x46, 0x0d, 0x4e, 0x27, 0x89, 0x4b, 0x5b, 0x74, 0x45, 0xa0, 0x9e, 0x71, 0x4c, 0x49,
+	0x29, 0x4a, 0x2d, 0x2e, 0x0e, 0x2e, 0x29, 0xca, 0xcc, 0x4b, 0x0f, 0x42, 0x28, 0x15, 0xb2, 0xe4,
+	0x62, 0x83, 0x38, 0x5e, 0x82, 0x49, 0x81, 0x51, 0x83, 0xdb, 0x48, 0x4c, 0x0f, 0x35, 0x1c, 0xf5,
+	0x20, 0xe6, 0x3b, 0x71, 0x9e, 0xb8, 0x27, 0xcf, 0xb0, 0xe2, 0xf9, 0x06, 0x2d, 0xc6, 0x20, 0xa8,
+	0x06, 0x2b, 0xe3, 0xa6, 0xe7, 0x1b, 0xb4, 0x10, 0x46, 0x75, 0x3d, 0xdf, 0xa0, 0xa5, 0x00, 0xf7,
+	0x0c, 0xcc, 0x3b, 0x68, 0xee, 0x54, 0x92, 0xe4, 0x12, 0x47, 0x13, 0x0a, 0x4a, 0x2d, 0x2e, 0xc8,
+	0xcf, 0x2b, 0x4e, 0x55, 0x52, 0xe4, 0x92, 0xf7, 0x2d, 0x4e, 0x0f, 0xcb, 0x2f, 0x49, 0x0d, 0x82,
+	0x78, 0x3a, 0x00, 0x64, 0x82, 0x5b, 0x6a, 0x6a, 0x0a, 0x5c, 0x89, 0x1a, 0x97, 0x0a, 0x54, 0x89,
+	0x1b, 0x2c, 0x84, 0x90, 0xd5, 0xc2, 0xd4, 0x19, 0xdd, 0x64, 0xe2, 0x62, 0xf6, 0x2d, 0x4e, 0x17,
+	0x8a, 0xe0, 0xe2, 0x41, 0x09, 0x25, 0x79, 0x74, 0xdf, 0xa1, 0xb9, 0x45, 0x4a, 0x9d, 0x80, 0x02,
+	0x98, 0x0d, 0x42, 0x05, 0x5c, 0x22, 0xd8, 0x5c, 0x2a, 0x84, 0xcd, 0x00, 0x6c, 0x0a, 0xa5, 0xf4,
+	0x89, 0x54, 0x08, 0xb7, 0xb1, 0x9e, 0x4b, 0x12, 0xa7, 0xc7, 0x85, 0x74, 0x70, 0x98, 0x86, 0x55,
+	0xb5, 0x94, 0x09, 0x29, 0xaa, 0x61, 0x0e, 0x90, 0x62, 0x6d, 0x00, 0x45, 0xbf, 0x93, 0xc1, 0x89,
+	0x47, 0x72, 0x8c, 0x17, 0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3,
+	0x85, 0xc7, 0x72, 0x0c, 0x37, 0x1e, 0xcb, 0x31, 0x44, 0x89, 0x61, 0xc4, 0x7e, 0x49, 0x65, 0x41,
+	0x6a, 0x71, 0x12, 0x1b, 0x38, 0xd9, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x7d, 0x6d, 0xe3,
+	0x88, 0xbc, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -173,6 +254,8 @@ type MsgClient interface {
 	// UpdateParams defines a (governance) operation for updating the module
 	// parameters. The authority defaults to the x/gov module account.
 	UpdateParams(ctx context.Context, in *MsgUpdateParams, opts ...grpc.CallOption) (*MsgUpdateParamsResponse, error)
+	VoteRequestPriceFeed(ctx context.Context, in *MsgVoteRequestPriceFeed, opts ...grpc.CallOption) (*MsgVoteRequestPriceFeedResponse, error)
+	VoteFinalizedRequestPrice(ctx context.Context, in *MsgVoteFinalizedRequestPrice, opts ...grpc.CallOption) (*MsgVoteFinalizedRequestPriceResponse, error)
 }
 
 type msgClient struct {
@@ -192,11 +275,31 @@ func (c *msgClient) UpdateParams(ctx context.Context, in *MsgUpdateParams, opts 
 	return out, nil
 }
 
+func (c *msgClient) VoteRequestPriceFeed(ctx context.Context, in *MsgVoteRequestPriceFeed, opts ...grpc.CallOption) (*MsgVoteRequestPriceFeedResponse, error) {
+	out := new(MsgVoteRequestPriceFeedResponse)
+	err := c.cc.Invoke(ctx, "/intellix.price.Msg/VoteRequestPriceFeed", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *msgClient) VoteFinalizedRequestPrice(ctx context.Context, in *MsgVoteFinalizedRequestPrice, opts ...grpc.CallOption) (*MsgVoteFinalizedRequestPriceResponse, error) {
+	out := new(MsgVoteFinalizedRequestPriceResponse)
+	err := c.cc.Invoke(ctx, "/intellix.price.Msg/VoteFinalizedRequestPrice", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	// UpdateParams defines a (governance) operation for updating the module
 	// parameters. The authority defaults to the x/gov module account.
 	UpdateParams(context.Context, *MsgUpdateParams) (*MsgUpdateParamsResponse, error)
+	VoteRequestPriceFeed(context.Context, *MsgVoteRequestPriceFeed) (*MsgVoteRequestPriceFeedResponse, error)
+	VoteFinalizedRequestPrice(context.Context, *MsgVoteFinalizedRequestPrice) (*MsgVoteFinalizedRequestPriceResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -205,6 +308,12 @@ type UnimplementedMsgServer struct {
 
 func (*UnimplementedMsgServer) UpdateParams(ctx context.Context, req *MsgUpdateParams) (*MsgUpdateParamsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateParams not implemented")
+}
+func (*UnimplementedMsgServer) VoteRequestPriceFeed(ctx context.Context, req *MsgVoteRequestPriceFeed) (*MsgVoteRequestPriceFeedResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method VoteRequestPriceFeed not implemented")
+}
+func (*UnimplementedMsgServer) VoteFinalizedRequestPrice(ctx context.Context, req *MsgVoteFinalizedRequestPrice) (*MsgVoteFinalizedRequestPriceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method VoteFinalizedRequestPrice not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -229,6 +338,42 @@ func _Msg_UpdateParams_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_VoteRequestPriceFeed_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgVoteRequestPriceFeed)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).VoteRequestPriceFeed(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/intellix.price.Msg/VoteRequestPriceFeed",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).VoteRequestPriceFeed(ctx, req.(*MsgVoteRequestPriceFeed))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Msg_VoteFinalizedRequestPrice_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgVoteFinalizedRequestPrice)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).VoteFinalizedRequestPrice(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/intellix.price.Msg/VoteFinalizedRequestPrice",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).VoteFinalizedRequestPrice(ctx, req.(*MsgVoteFinalizedRequestPrice))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var Msg_serviceDesc = _Msg_serviceDesc
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "intellix.price.Msg",
@@ -237,6 +382,14 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UpdateParams",
 			Handler:    _Msg_UpdateParams_Handler,
+		},
+		{
+			MethodName: "VoteRequestPriceFeed",
+			Handler:    _Msg_VoteRequestPriceFeed_Handler,
+		},
+		{
+			MethodName: "VoteFinalizedRequestPrice",
+			Handler:    _Msg_VoteFinalizedRequestPrice_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -306,6 +459,52 @@ func (m *MsgUpdateParamsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error)
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgVoteRequestPriceFeedResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgVoteRequestPriceFeedResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgVoteRequestPriceFeedResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgVoteFinalizedRequestPriceResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgVoteFinalizedRequestPriceResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgVoteFinalizedRequestPriceResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -333,6 +532,24 @@ func (m *MsgUpdateParams) Size() (n int) {
 }
 
 func (m *MsgUpdateParamsResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgVoteRequestPriceFeedResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgVoteFinalizedRequestPriceResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -489,6 +706,106 @@ func (m *MsgUpdateParamsResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgUpdateParamsResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgVoteRequestPriceFeedResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgVoteRequestPriceFeedResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgVoteRequestPriceFeedResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgVoteFinalizedRequestPriceResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgVoteFinalizedRequestPriceResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgVoteFinalizedRequestPriceResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
