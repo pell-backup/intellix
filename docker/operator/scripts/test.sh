05d7dc0cb4236c35366fd22af6a5fed5539f9003
jimmy <jimmy>
2024-11-21 20:30:24 +0800

    e2e:  update operator container test scripts

--- docker/operator/scripts/test.sh
+++ docker/operator/scripts/test.sh
@@ -48,7 +48,7 @@ export TIMEOUT_FOR_TASK_PROCESS=${TIMEOUT_FOR_TASK_PROCESS:-8}
 export TIMEOUT_FOR_TASK_PROCESS=$TIMEOUT_FOR_TASK_PROCESS
 echo "wait ${TIMEOUT_FOR_TASK_PROCESS} seconds for the task to be processed"
 sleep ${TIMEOUT_FOR_TASK_PROCESS}
-RESULT=$(cast call "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "price()" --private-key "$ADMIN_KEY"
+RESULT=$(cast call "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "price()" --private-key "$ADMIN_KEY" | cast to-dec
 assert_eq "$RESULT" "0"
 
 # cast call "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "allTaskResponses(uint32)" $((TASK_NUMBER - 1))
