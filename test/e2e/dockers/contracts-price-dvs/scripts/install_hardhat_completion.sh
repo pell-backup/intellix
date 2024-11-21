9ca2a44dad4027d95faa59d50df0af8fcc40d2ef
jimmy <jimmy>
2024-11-21 15:40:20 +0800

    remove unused code

--- test/e2e/dockers/contracts-price-dvs/scripts/install_hardhat_completion.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect
-set time 10
-spawn hardhat-completion install
-expect "Which Shell do you use ?"
-send -- "bash\r"
-expect "We will install completion to ~/.bashrc, is it ok ? (y/N)"
-send -- "y\r"
-expect eof
