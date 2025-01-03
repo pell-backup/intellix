d446e843eb5c0fee58f8466c45748729ea4ac96e
jimmy <jimmy>
2025-01-03 21:52:45 +0800

    cd/testnet: update operator/setup_operator_key.sh

--- docker/devnet/operator/setup_operator_key.sh
+++ docker/devnet/operator/setup_operator_key.sh
@@ -7,15 +7,17 @@ function load_defaults {
 }
 
 function setup_operator_key {
-  # echo  -ne '\n\n' | pelldvs keys create operator --key-type=ecdsa --insecure > /tmp/operator.key
-  if ! pelldvs keys show operator --home "$PELLDVS_HOME" >/dev/null 2>&1; then
+
+  # if "$PELLDVS_HOME"/keys/operator.ecdsa.key.json not exists then import it
+  if [ ! -f "$PELLDVS_HOME"/keys/operator.ecdsa.key.json ]; then
     echo -ne '\n\n' | pelldvs keys import --key-type ecdsa --insecure operator $OPERATOR_KEY --home $PELLDVS_HOME >/dev/null
   fi
 
   export OPERATOR_ADDRESS=$(pelldvs keys show operator --home $PELLDVS_HOME | awk '/Key content:/{getline; print}' | head -n 1 | jq -r .address)
 
   ## To register operator in the DVS, we need the operator's BLS key with the same name
-  if ! pelldvs keys show operator --key-type bls --home "$PELLDVS_HOME" >/dev/null 2>&1; then
+  # if "$PELLDVS_HOME"/keys/operator.bls.key.json not exists then create it
+  if [ ! -f "$PELLDVS_HOME"/keys/operator.bls.key.json ]; then
     echo  -ne '\n\n' | pelldvs keys create operator --key-type=bls --insecure > /tmp/operator_bls.key
   fi
 
