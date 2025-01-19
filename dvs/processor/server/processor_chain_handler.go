3217051f1f67e71ddfa7b7b2a83e807d688161ee
kai <kai@>
2025-01-19 11:49:48 +0800

    feat: chain listener init

--- dvs/processor/server/processor_chain_handler.go
+++ dvs/processor/server/processor_chain_handler.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	context "context"
+	"fmt"
 	processortypes "intellix/x/processor/types"
 
 	abci "github.com/cometbft/cometbft/abci/types"
@@ -10,8 +11,59 @@ import (
 )
 
 func (r *RequestServer) ProcessorEventHandler(ctx context.Context, event abci.Event) (string, *processortypes.MsgVoteRequestProcessor, error) {
+	var requestId string
+	var operatorId string
 
-	return "", nil, nil
+	for _, attr := range event.Attributes {
+		switch attr.Key {
+		case "request_id":
+			requestId = attr.Value
+		case "operator_id":
+			operatorId = attr.Value
+		}
+	}
+
+	processor, err := r.queryVoteRequestProcessor(ctx, []byte(requestId), operatorId)
+	if err != nil {
+		return "", nil, err
+	}
+
+	r.logger.Info("receive processor event", "processor", fmt.Sprintf("%+v", processor))
+
+	return requestId, processor, nil
+}
+
+func (r *RequestServer) queryVoteRequestProcessor(ctx context.Context, requestId []byte, operatorId string) (*processortypes.MsgVoteRequestProcessor, error) {
+	conn := r.clientCtx.GRPCClient
+	queryClient := processortypes.NewQueryClient(conn)
+
+	req := &processortypes.QueryVoteRequestProcessorRequest{
+		RequestId:  requestId,
+		OperatorId: operatorId,
+	}
+
+	resp, err := queryClient.QueryVoteRequestProcessor(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &processortypes.MsgVoteRequestProcessor{
+		TaskIndex:                 resp.TaskIndex,
+		RequestId:                 resp.RequestId,
+		FeeToken:                  resp.FeeToken,
+		Payment:                   resp.Payment,
+		RequestData:               resp.RequestData,
+		CallbackAddress:           resp.CallbackAddress,
+		CallbackFunctionId:        resp.CallbackFunctionId,
+		TaskCreatedBlock:          resp.TaskCreatedBlock,
+		QuorumNumbers:             resp.QuorumNumbers,
+		QuorumThresholdPercentage: resp.QuorumThresholdPercentage,
+		ScriptId:                  resp.ScriptId,
+		ScriptResp:                resp.ScriptResp,
+		Sender:                    resp.Sender,
+		OperatorId:                resp.OperatorId,
+		BlsSignature:              resp.BlsSignature,
+	}, nil
 }
 
 func (r *RequestServer) ProcessorBlockHandler(ctx context.Context, block *cmttypes.Block) (string, *processortypes.MsgVoteRequestProcessor, error) {
--- x/processor/types/query.pb.gw.go
+++ x/processor/types/query.pb.gw.go
@@ -81,6 +81,23 @@ func request_Query_ListProcessor_0(ctx context.Context, marshaler runtime.Marsha
 
 }
 
+var (
+	filter_Query_QueryVoteRequestProcessor_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Query_QueryVoteRequestProcessor_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryVoteRequestProcessorRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Query_QueryVoteRequestProcessor_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.QueryVoteRequestProcessor(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterQueryHandlerFromEndpoint is same as RegisterQueryHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterQueryHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -179,6 +196,26 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Query_QueryVoteRequestProcessor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Query_QueryVoteRequestProcessor_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_QueryVoteRequestProcessor_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -188,6 +225,8 @@ var (
 	pattern_Query_ShowProcessor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"intellix", "processor", "id"}, ""))
 
 	pattern_Query_ListProcessor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 1}, []string{"intellix", "processor"}, ""))
+
+	pattern_Query_QueryVoteRequestProcessor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"intellix", "processor", "vote_request_processor"}, ""))
 )
 
 var (
@@ -196,4 +235,6 @@ var (
 	forward_Query_ShowProcessor_0 = runtime.ForwardResponseMessage
 
 	forward_Query_ListProcessor_0 = runtime.ForwardResponseMessage
+
+	forward_Query_QueryVoteRequestProcessor_0 = runtime.ForwardResponseMessage
 )
