04062e4e6a8dfd1bd4f422c1a61648d00a68e72f
jimmy <jimmy@jimmyzai.com>
2025-03-06 17:35:22 +0800

    remove unsed scripts/docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -d "$PELLDVS_HOME/config" ]; then
-	echo "Running pelldvs init to create (default) configuration for docker run."
-	pelldvs init
-fi
-
-exec intellixd "$@"
