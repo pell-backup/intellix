3fd89e45a27b607431d7c8d966ffdbc3207823b9
smallyunet <dev.wangyu@gmail.com>
2025-03-19 15:09:59 +0800

    fix e2e error
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- docker/operator/scripts/test.sh
+++ docker/operator/scripts/test.sh
@@ -62,8 +62,6 @@ PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS=$(ssh hardhat "cat $HARDHAT_DVS_PATH
 
 export TIMEOUT_FOR_TASK_PROCESS=${TIMEOUT_FOR_TASK_PROCESS:-20}
 
-export TIMEOUT_FOR_TASK_PROCESS=${TIMEOUT_FOR_TASK_PROCESS:-20}
-
 ### ----------------
 ## create a new task
 cast send "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "requestPrice(string)" "ETH" --private-key "$ADMIN_KEY" --rpc-url "$ETH_RPC_URL"
@@ -84,12 +82,6 @@ logt "update operator sokcet address and sleep 5 seconds to wait for the dispatc
 ssh operator "bash /root/scripts/update_operator_socket.sh http://operator:26657"
 logt ""
 
-# update operator socket address, in init_operator.sh we have set the operator socket address to http://$(hostname -i):26657
-# here we update it to http://operator:26657
-logt "update operator sokcet address and sleep 5 seconds to wait for the dispatcher has processed it "
-ssh operator "bash /root/scripts/update_operator_socket.sh http://operator:26657"
-logt ""
-
 ### ----------------
 ## create a new task
 cast send "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "requestPrice(string)" "BTC" --private-key "$ADMIN_KEY" --rpc-url "$ETH_RPC_URL"
@@ -101,11 +93,10 @@ RESULT=$(cast call "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "price()" --pr
 assert_gt "$RESULT" "0"
 
 ### ----------------
+## create a new vrf task
+VRF_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS=$(ssh hardhat "cat $HARDHAT_DVS_PATH/VRFOraclePayInNativeConsumer.json" | jq -r .address)
+
+cast send "$VRF_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "requestRandomWords(uint256)" 1 --private-key "$ADMIN_KEY" --rpc-url "$ETH_RPC_URL"
 
 
 
-# wait for the task to be processed
-echo "wait ${TIMEOUT_FOR_TASK_PROCESS} seconds for the task to be processed"
-sleep ${TIMEOUT_FOR_TASK_PROCESS}
-RESULT=$(cast call "$PRICE_ORACLE_PAY_IN_NATIVE_CONSUMER_ADDRESS" "price()" --private-key "$ADMIN_KEY" --rpc-url "$ETH_RPC_URL" | cast to-dec)
-assert_gt "$RESULT" "0"
