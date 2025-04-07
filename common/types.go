c167fe41a1da4ba65637156fbe91ec0e281ec542
smallyunet <dev.wangyu@gmail.com>
2025-04-07 17:11:28 +0800

    fix
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- common/types.go
+++ common/types.go
@@ -2,6 +2,6 @@ package common
 
 // ECCKeyPair is a struct that contains ECC private key and public key
 type ECCKeyPair struct {
-	ECCPrivateKey string `json:"ecc_private_key" mapstructure:"ecc_private_key"`
-	ECCPublicKey  string `json:"ecc_public_key" mapstructure:"ecc_public_key"`
+	ECCPrivateKeyPath string `json:"ecc_private_key_path" mapstructure:"ecc_private_key_path"`
+	ECCPublicKey      string `json:"ecc_public_key" mapstructure:"ecc_public_key"`
 }
